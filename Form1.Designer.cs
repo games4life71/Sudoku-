31bd7c78358a6102488623ac37264c94f5ac8236
stefan_adrian <39777353+games4life71@users.noreply.github.com>
2020-09-25 20:57:25 +0300

    first commit
    randomly generate the map and the numbers

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace Sudoku
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel.ColumnCount = 9;
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 65F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 67F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 72F));
+            this.panel.Location = new System.Drawing.Point(201, 12);
+            this.panel.Margin = new System.Windows.Forms.Padding(1, 3, 1, 1);
+            this.panel.Name = "panel";
+            this.panel.RowCount = 9;
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 66F));
+            this.panel.Size = new System.Drawing.Size(600, 600);
+            this.panel.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1056, 643);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel panel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sudoku
+{
+    public partial class Form1 : Form
+    {
+
+        private static int[,] virtualMap = new int[9, 9];
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateMap(panel);
+            GenerateNumbers(virtualMap, panel);
+        }
+         public static void GenerateMap(TableLayoutPanel panel )
+        {
+            int ColorChange = 1;
+            bool isGrey = true;
+            for(int i = 0; i<panel.RowCount;i++)
+                for(int j = 0; j<panel.ColumnCount;j++)
+                {
+                    Button btn = new Button();
+                   
+                    btn.Margin = new Padding(0);
+                    btn.Dock = DockStyle.Fill;
+                   //color the butons 
+                  
+
+
+                    panel.Controls.Add(btn, j, i);
+
+                }
+            for (int i = 0; i < panel.RowCount - 2; i += 3)
+                for (int j = 0; j < panel.ColumnCount - 2; j+=3)
+                {
+                    if (ColorChange % 2 == 0)
+                    {
+                        panel.GetControlFromPosition(j, i).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j, i + 1).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j, i + 2).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+1, i).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+2, i ).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j, i ).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+1, i+1 ).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+2, i+1 ).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+1, i+2 ).BackColor = Color.DarkGray;
+                        panel.GetControlFromPosition(j+2, i+2 ).BackColor = Color.DarkGray;
+                        
+                       
+                        ColorChange++;
+                    }
+                    else
+                    {
+                       
+                        ColorChange++;
+                    }
+                }
+                
+
+
+        }
+        public static void GenerateNumbers(int[,] virtualMap, TableLayoutPanel panel )
+        {
+
+
+            Random rnd = new Random();
+            int numbersToPick = 3;
+
+            for (int i = 0; i < panel.RowCount - 3; i += 3)
+                for (int j = 0; j < panel.ColumnCount - 3; j += 3)
+                {
+                        numbersToPick = 4;
+                    List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                    while (numbersToPick > 0)
+                    {
+                        int pickedPos = rnd.Next(0, digits.Count -1 );
+                        int pickedNumber = digits[pickedPos];
+                        digits.RemoveAt(pickedPos);
+                        int rnd_x = rnd.Next(j, j + 3);
+                        int rnd_y = rnd.Next(i, i + 3);
+                        numbersToPick--;
+
+                        //add to the map and tablepanel 
+                        panel.GetControlFromPosition(rnd_x, rnd_y).Text = pickedNumber.ToString();
+                        virtualMap[rnd_x, rnd_y] = pickedNumber;
+
+                    }
+
+
+                }
+
+                }
+
+        public static bool isEligible ( int  number , int[,] map, ValueTuple<int, int> pos  )
+        {
+            
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //initialize the map 
+        }
+
+        private void flowLayoutPanel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}

5897bd6176e6b502f944b951607548fb2cc82093
stefan_adrian <39777353+games4life71@users.noreply.github.com>
2020-09-26 16:20:40 +0300

    Add files via upload
    -improved map generating algorithm

--- Form1.cs
+++ Form1.cs
@@ -18,6 +18,7 @@ namespace Sudoku
         public Form1()
         {
             InitializeComponent();
+           
             GenerateMap(panel);
             GenerateNumbers(virtualMap, panel);
         }
@@ -32,6 +33,7 @@ namespace Sudoku
                    
                     btn.Margin = new Padding(0);
                     btn.Dock = DockStyle.Fill;
+                    btn.Font = new Font("Arial", 12, FontStyle.Bold);
                    //color the butons 
                   
 
@@ -75,38 +77,94 @@ namespace Sudoku
             Random rnd = new Random();
             int numbersToPick = 3;
 
-            for (int i = 0; i < panel.RowCount - 3; i += 3)
-                for (int j = 0; j < panel.ColumnCount - 3; j += 3)
+            for (int i = 0; i < panel.RowCount - 1; i += 3)
+                for (int j = 0; j < panel.ColumnCount - 1; j += 3)
                 {
-                        numbersToPick = 4;
+                    Console.WriteLine();
+                        numbersToPick = 5;
                     List<int> digits = new List<int> { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
                     while (numbersToPick > 0)
-                    {
+                    {     
                         int pickedPos = rnd.Next(0, digits.Count -1 );
                         int pickedNumber = digits[pickedPos];
-                        digits.RemoveAt(pickedPos);
                         int rnd_x = rnd.Next(j, j + 3);
                         int rnd_y = rnd.Next(i, i + 3);
+                        bool eligible = false;
+                        while (eligible == false )
+                        {
+
+                            if (isEligible(pickedNumber, virtualMap, (rnd_x, rnd_y)))
+                            {
+                                
+                                //then number is good to go 
+
+
+                                panel.GetControlFromPosition(rnd_x, rnd_y).Text = pickedNumber.ToString();
+                                virtualMap[rnd_x, rnd_y] = pickedNumber;
+                                digits.RemoveAt(pickedPos);
+                                eligible = true; 
+                            }
+                            else
+                            {
+                                //reselect the number 
+                                pickedPos = rnd.Next(0, digits.Count - 1);
+                                pickedNumber = digits[pickedPos];
+                                rnd_x = rnd.Next(j, j + 3);
+                                rnd_y = rnd.Next(i, i + 3);
+
+                            }
+                        }
                         numbersToPick--;
 
                         //add to the map and tablepanel 
-                        panel.GetControlFromPosition(rnd_x, rnd_y).Text = pickedNumber.ToString();
-                        virtualMap[rnd_x, rnd_y] = pickedNumber;
 
                     }
 
 
                 }
 
+                    ShowMap(virtualMap);
                 }
+        public static void ShowMap (int[,] map )
+        {
+
+            for (int i = 0; i < 9;i++)
+            {
+                for (int j = 0; j < 9; j++)
+                    Console.Write(map[i, j]+ " ");
+
+            }
+
+            Console.WriteLine();
+        }
+        public static void ClearMap(int[,] map, TableLayoutPanel panel)
+        {
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    map[i, j] = 0;
+                    panel.GetControlFromPosition(j, i).Text = null;
+
+                }
+
+            }
+        }
 
         public static bool isEligible ( int  number , int[,] map, ValueTuple<int, int> pos  )
         {
-            
+            for (int i  = 0; i<9;i++)
+            {
+                if (map[i, pos.Item2] == number || map[pos.Item1, i] == number) return false;
+                
+            }
 
-
+            return true; 
 
         }
+
+        
 
         private void Form1_Load(object sender, EventArgs e)
         {
@@ -121,6 +179,16 @@ namespace Sudoku
         private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
         {
 
+        }
+
+        private void restartGameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to restart ? ", "Restart Game" , MessageBoxButtons.YesNo , MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                ClearMap(virtualMap  , panel);
+                GenerateNumbers(virtualMap, panel);
+
+            }
         }
     }
 }
